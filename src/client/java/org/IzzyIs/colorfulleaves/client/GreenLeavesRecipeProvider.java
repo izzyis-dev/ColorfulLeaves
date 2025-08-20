d8eeeac431604ca41b511a7b045aaa54b1a04a45
IzzyIs <iwilkinsthegamer@gmail.com>
2025-08-20 16:22:14 -0400

    first commit

---
+++ src/client/java/org/IzzyIs/colorfulleaves/client/GreenLeavesRecipeProvider.java
@@ -0,0 +1,43 @@
+package org.IzzyIs.colorfulleaves.client;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+import org.IzzyIs.colorfulleaves.ModBlocks;
+
+import java.util.concurrent.CompletableFuture;
+
+public class GreenLeavesRecipeProvider extends FabricRecipeProvider {
+    public GreenLeavesRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registryLookup, RecipeExporter exporter) {
+        return new RecipeGenerator(registryLookup, exporter) {
+            @Override
+            public void generate() {
+                @SuppressWarnings("unused")
+                RegistryWrapper.Impl<Item> itemLookup = registries.getOrThrow(RegistryKeys.ITEM);
+
+                createShapeless(RecipeCategory.DECORATIONS, ModBlocks.GREEN_LEAF_BLOCK, 1)
+                        .input(Items.GREEN_DYE).input(ItemTags.LEAVES)
+                        .criterion(hasItem(Items.GREEN_DYE), conditionsFromItem(Items.GREEN_DYE))
+                        .offerTo(exporter);
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "GreenLeavesRecipeProvider";
+    }
+
+}
